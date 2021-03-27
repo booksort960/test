e07742ad6948890e90029bca10280811a99ab86e
booksort960 <2141166187@qq.com>
2021-03-27 23:43:23 +0800

    C预处理的草稿

---
+++ blog.c
@@ -0,0 +1,16 @@
+//#define SUM 2+2
+//int num = SUM * SUM;
+//#define FOUR 2*2
+//#define FOURS 2 * 2
+#include <stdio.h>
+#define	SQUARE(X) X*X
+#define MAX 10
+#define PSQR(X) printf("X is %d\n",((X)*(X));
+#define PSQR(X) printf("#X" is %d\n",((X)*(X));
+
+int main(void)
+{
+
+	printf("%d", MAX+MAX);
+	return 0;
+}
