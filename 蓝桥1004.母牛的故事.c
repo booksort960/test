753bc5e1730600e7f4e3ffd609cbda41e0d33f7b
booksort960 <2141166187@qq.com>
2021-04-17 23:49:22 +0800

    。

---
+++ 蓝桥1004.母牛的故事.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int recursion(int n)
+{
+	if(n<4)
+		 
+
+}
+int main(void)
+{
+	int n;
+	while (~scanf_s("%d", &n))
+	{
+		int num=recursion(n);
+		if(num)
+			printf("%d", num);
+	}
+	return 0;
+}
